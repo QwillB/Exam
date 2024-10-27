0d073c06d50baf54f7f4261ae557fd520efaa94f
User <User@QWILL>
2024-10-27 21:10:37 +0300

    Добавьте файлы проекта.

---
+++ Exam/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Exam
+{
+    public partial class MainWindow : Window
+    {
+        private bool _isDragging = false;
+        private Point _dragStartPoint;
+        private Vector _offset;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _isDragging = true;
+                _dragStartPoint = e.GetPosition(this); // Получаем начальную позицию
+                _offset = new Vector(Mouse.GetPosition(this).X, Mouse.GetPosition(this).Y); // Определяем смещение
+                Mouse.Capture(this); // Захватываем мышь
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDragging)
+            {
+                Point mousePos = e.GetPosition(this);
+
+                this.Left += mousePos.X - _dragStartPoint.X;
+                this.Top += mousePos.Y - _dragStartPoint.Y;
+            }
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+            if (e.ButtonState == MouseButtonState.Released)
+            {
+                _isDragging = false;
+                Mouse.Capture(null);
+            }
+        }
+    }
+}
---
+++ Exam/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+using Exam.Pages;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Exam
+{
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void AuthorizeButton_Click(object sender, RoutedEventArgs e)//при нажатии на кнопку идет заполнение данных текущего пользователя, если такой зарегестрирован
+        {
+            bool userExists = DataAccessLayer.UserAuthorization(authorizationLoginTextBox.Text, authorizationPasswordTextBox.Password);
+            if (userExists)
+            {
+                CurrentUser.IsGuest = false;
+                NavigationService.Navigate(new ShopPage());
+            }
+            else
+                IncorrectDataLabel.Visibility = Visibility.Visible;
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentUser.IsGuest = true;
+            NavigationService.Navigate(new ShopPage());
+        }
+
+        private void AuthorizationPasswordTextBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            IncorrectDataLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void AuthorizationLoginTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            IncorrectDataLabel.Visibility = Visibility.Hidden;
+        }
+        private void CloseImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            if (parentWindow != null) 
+            {
+                parentWindow.Close();
+            }
+        }
+
+    }
+}
